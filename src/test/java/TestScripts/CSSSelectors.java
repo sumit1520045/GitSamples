eaae1513a18ed2ee4a1a0c30b4aa43861e7bf7bf
Administrator <Administrator@172.31.1.82>
2023-12-04 06:24:33 +0000

    added new java fie

---
+++ src/test/java/TestScripts/CSSSelectors.java
@@ -0,0 +1,24 @@
+package TestScripts;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CSSSelectors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		WebElement srcBox = driver.findElement(By.name("q"));
+		srcBox.sendKeys("learn selenium");
+		System.out.println("Title....."+driver.getTitle());
+		srcBox.sendKeys(Keys.ENTER);
+	//comment
+
+		
+	}
+
+}
